1a5f3b6e35f0fba881eb7f0e7fef80815eef4a7a
TriptiRajpuut <167672637+TriptiRajpuut@users.noreply.github.com>
2026-01-20 11:58:05 +0530

    Add files via upload

---
+++ pattern5.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            if (i < n - 1) {
+                System.out.println("*");
+            } else {
+                for (int j = 0; j < n; j++) {
+                    System.out.print("* ");
+                }
+            }
+        }
+    }
+}
